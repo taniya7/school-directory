8e55cac1c729d2bc056ab89f81f515d87cc61d1b
Taniya Sarnobat <taniyasarnobat07@gmail.com>
2025-09-04 23:29:41 +0530

    handling large files

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 
 const pool = mysql.createPool({
