96579f31fe7412845c8dd4b6dff6ef9568a07520
Taniya Sarnobat <taniyasarnobat07@gmail.com>
2025-09-04 21:44:52 +0530

    debugging

--- client/src/utils/schoolsData.js
+++ client/src/utils/schoolsData.js
@@ -2,12 +2,14 @@ import { create } from "zustand";
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+console.log("API_URL in prod:", API_URL);
 
 export const useSchoolStore = create((set, get) => ({
   schools: [],
   fetchSchools: async () => {
     try {
       const res = await axios.get(`${API_URL}/schools`);
+      console.log("Fetched schools:", res.data);
       set({ schools: res.data.data });
     } catch (err) {
       console.error("Error fetching schools:", err);
